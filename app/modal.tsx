b1c9ca2f3af079329b5bde817efc461d84742b0c
tdat3023 <tdat3023@gmail.com.com>
2024-05-23 19:51:27 +0700

    Test auto merge master to main

--- app/modal.tsx
+++ app/modal.tsx
@@ -10,7 +10,9 @@ export default function ModalScreen() {
       <Text style={styles.title}>Modal Test merger master</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/modal.tsx" />
-      <Text style={styles.title}>Bỏ cờ all watch</Text>
+      {/* <Text style={styles.title}>Bỏ cờ all watch</Text> */}
+      <Text style={styles.title}>Test auto merge master to main</Text>
+
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
